d4a26f9a52cb1c43ee8cda01d5b359cb79a28588
hrfmartins <hugo.martins365@gmail.com>
2022-09-25 11:56:25 +0200

    added example project

---
+++ src/pt/example/hm/Main.java
@@ -0,0 +1,11 @@
+package pt.example.hm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static String substring(String hello, Integer start, Integer end) {
+        return hello.substring(start, end);
+    }
+}
